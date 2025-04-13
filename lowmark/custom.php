8ffde4a40adb0b635d0ab307c0bc485a97a71573
weitblick <emk@weitblick.de>
2025-04-13 16:05:32 +0200

    clean custom.php

--- lowmark/custom.php
+++ lowmark/custom.php
@@ -1,21 +1,6 @@
 <?php
 
-// ============ CUSTOM CODE LOEWENSTEIN ================
+// ============ ADD CUSTOM CODE ================
 
-function has_prayers($content) { // NOT USED
-    $has_prayers = false;
-
-    // Ersetze "## Fürbitten" und speichere das Ergebnis
-    $new_content = preg_replace('/## Fürbitten/', "## Fürbitten{#fuerbitten}", $content);
-
-    // Prüfe, ob eine Ersetzung stattgefunden hat
-    if ($new_content !== $content) {
-        $has_prayers = true;
-        $content = $new_content; // Aktualisiere den Inhalt
-    }
-
-    return ['has_prayers' => $has_prayers, 'content' => $content];
-
-}
 
 ?>
